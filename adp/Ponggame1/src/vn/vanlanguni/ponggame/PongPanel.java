1ca866b6394c5b956c614ac7f848500d25e551f4
Minh <tnnm.vtn@gmail.com>
2016-11-09 01:05:24 +0700

    Fixed #16 Draw player2 scores is wrong

--- adp/Ponggame1/src/vn/vanlanguni/ponggame/PongPanel.java
+++ adp/Ponggame1/src/vn/vanlanguni/ponggame/PongPanel.java
@@ -242,7 +242,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			g.setColor(Color.BLUE);
 			g.drawString(String.valueOf(playerOneScore), 100, 100); // Player 1
 																	// score
-			g.drawString(String.valueOf(playerOneScore), 350, 100); // Player 2
+			g.drawString(String.valueOf(playerTwoScore), 350, 100); // Player 2
 																	// score
 
 			// draw the ball
