b9095d558bdb16e0ec82132c7aa01d7bfded4cf4
My Phan <tramy290997@gmail.com>
2016-11-29 18:30:09 +0700

    sound

--- adp/Ponggame1/src/vn/vanlanguni/ponggame/PongPanel.java
+++ adp/Ponggame1/src/vn/vanlanguni/ponggame/PongPanel.java
@@ -187,7 +187,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 
 			// ball bounces off top and bottom of screen
 			if (nextBallTop < 0 || nextBallBottom > getHeight()) {
-				//Sound.play("sound/bounce.wav");
+				Sound.play("sound/bounce.wav");
 				ballDeltaY *= -1;
 			}
 
@@ -195,7 +195,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 			if (nextBallLeft < playerOneRight) {
 				// is it going to miss the paddle?
 				if (nextBallTop > playerOneBottom || nextBallBottom < playerOneTop) {
-					//Sound.play("sound/ht.wav");
+					Sound.play("sound/ht.wav");
 					playerTwoScore++;
 
 					// Player 2 Win, restart the game
@@ -208,7 +208,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 				} else {
 					// If the ball hitting the paddle, it will bounce back
 					// FIXME Something wrong here
-					//Sound.play("sound/hehe.wav");
+					Sound.play("sound/hehe.wav");
 					ballDeltaX *= -1;
 					lastHitBall = 1;
 				}
@@ -218,7 +218,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 			if (nextBallRight > playerTwoLeft) {
 				// is it going to miss the paddle?
 				if (nextBallTop > playerTwoBottom || nextBallBottom < playerTwoTop) {
-					//Sound.play("sound/ht.wav");
+					Sound.play("sound/ht.wav");
 					playerOneScore++;
 
 					// Player 1 Win, restart the game
@@ -232,7 +232,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener, Mo
 
 					// If the ball hitting the paddle, it will bounce back
 					// FIXME Something wrong here
-					//Sound.play("sound/hehe.wav");
+					Sound.play("sound/hehe.wav");
 					ballDeltaX *= -1;
 					lastHitBall = 2 ;
 				}
